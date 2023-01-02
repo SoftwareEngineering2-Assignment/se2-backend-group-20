2a0c2b361a7e77d3af0fac3b444465fca7e33be6
Christos <giachoud@gapps.auth.gr>
2023-01-02 15:48:50 +0200

    Routes testing begins

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,19 +1,28 @@
 /* eslint-disable import/no-unresolved */
-const test = require('ava').default;
 
-test('Routing test', (t) => {
-  t.pass();
+// prepare for testing and do the needed imports
+const test = require('ava').default;
+const listen = require('test-listen');
+const got = require('got');
+const http = require('node:http');
+const app = require('../src/index');
+
+// before running the tests setup the test environment
+test.before(async (t) => {
+  t.context.server = http.createServer(app);
+  t.context.prefixUrl = await listen(t.context.server);
+  t.context.got = got.extend({ http2: true, throwHttpErrors: false, responseType: 'json', prefixUrl: t.context.prefixUrl })
 });
 
-// test('Test value', async (t) => {
-//   const a = 1;
-//   t.is(a + 1, 2);
-// });
+// after the test close all the pending matters
+test.after((t) => {
+  t.context.server.close();
+});
 
-// const sum = (a, b) => a + b;
 
-// test('Sum of 2 numbers', (t) => {
-//   t.plan(2);
-//   t.pass('this assertion passed');
-//   t.is(sum(1, 2), 3);
-// });
+test('GET | /statistics | returns correct status code', async (t) => {
+  const {statusCode, body} = await t.context.got('general/statistics');
+  t.is(statusCode, 200);
+  t.assert(body.success);
+  t.is(body.source, 1);
+});
