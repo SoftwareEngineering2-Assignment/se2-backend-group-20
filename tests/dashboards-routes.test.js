ce377bf539adcf1d9231ba0f590c7d3ea0841877
Christos <giachoud@gapps.auth.gr>
2023-01-06 14:03:59 +0200

    Routes testing update

--- tests/dashboards-routes.test.js
+++ tests/dashboards-routes.test.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-unresolved */
-
 // prepare for testing and do the needed imports
 const test = require('ava').default;
 const listen = require('test-listen');
@@ -22,45 +20,14 @@ test.after((t) => {
 });
 
 /*
-  Tests for general routes
-*/
-
-test('GET | /statistics | returns correct status code', async (t) => {
-  const {statusCode, body} = await t.context.got('general/statistics');
-  t.is(statusCode, 200);
-  t.assert(body.success);
-  t.is(body.sources, 0);
-});
-
-test('GET | /sources', async (t) => {
-  const token = jwtSign({ username: "karanikio", id: "603240b6bc3cb735d09ee880", email: 'karanikio@auth.gr' });
-  const { statusCode, body } = await t.context.got(`sources/sources?token=${token}`);
-  t.is(statusCode, 200);
-});
-
-test('GET | /test-url', async (t) => {
-  const { statusCode } = await t.context.got('general/test-url');
-  t.is(statusCode, 200);
-});
-
-test('GET | /test-url-request', async (t) => {
-  const { statusCode, body } = await t.context.got('general/test-url-request');
-  t.is(statusCode, 200);
-  console.log(body);
-});
-
-
-
-/*
   Tests for dashboards routes
 */
 
-// test('POST | /create-dashboard', async (t) => {
-//   const token = jwtSign({ username: "karanikio", id: "603240b6bc3cb735d09ee880", email: 'karanikio@auth.gr' });
-//   const { statusCode, body } = await t.context.got(`dashboards/create-dashboard?token=${token}`, { name: "mydash" });
-//   console.log(body.success);
-//   t.assert(body.success);
-// });
+test('POST | /create-dashboard', async (t) => {
+  const token = jwtSign({ username: "karanikio", id: "603240b6bc3cb735d09ee880", email: 'karanikio@auth.gr' });
+  const { statusCode, body } = await t.context.got(`dashboards/create-dashboard?token=${token}`, { name: "mydash" });
+  t.assert(body.success);
+});
 
 // test('GET | /dashboards', async (t) => {
 //   const token = jwtSign({ username: "karanikio", id: "603240b6bc3cb735d09ee880", email: 'karanikio@auth.gr' });
