492307eb885114c126e824ec850c978f408adc32
Christos <giachoud@gapps.auth.gr>
2023-01-25 18:34:29 +0200

    Backend testing functional at last

--- tests/routes_testing.test.js
+++ tests/routes_testing.test.js
@@ -29,7 +29,7 @@ test('GET | /statistics | returns correct status code', async (t) => {
   const {statusCode, body} = await t.context.got('general/statistics');
   t.is(statusCode, 200);
   t.assert(body.success);
-  t.is(body.sources, 0);
+  t.is(body.sources, 1)
 });
 
 test('GET | /test-url', async (t) => {
@@ -49,7 +49,7 @@ test('GET | /test-url-request', async (t) => {
 //   Tests for dashboards routes
 // */
 
-// First create a dashboard
+// First create a dashboard (working)
 // test('POST | /create-dashboard', async (t) => {
 //   const token = jwtSign({ username: process.env.USERNAME, id: process.env.ID, email: process.env.EMAIL });
 //   const dashboard_name = {name: "mydash4"}
@@ -58,7 +58,7 @@ test('GET | /test-url-request', async (t) => {
 //   console.log(answer.body)
 // });
 
-//Then find the created dashboard
+//Then find the created dashboard (working)
 test('GET | /dashboards', async (t) => {
   const token = jwtSign({ username: process.env.USERNAME, id: process.env.ID, email: process.env.EMAIL });
   const { statusCode, body } = await t.context.got(`dashboards/dashboards?token=${token}`);
@@ -66,7 +66,7 @@ test('GET | /dashboards', async (t) => {
   console.log(body.dashboards);
 });
 
-// Finally delete the dashboard
+// Finally delete the dashboard (working) (if not working maybe change the id of the dashboard)
 // test('POST | /delete-dashboard', async (t) => {
 //   const token = jwtSign({ username: process.env.USERNAME, id: process.env.ID, email: process.env.EMAIL });
 //   const dash_id = { id: "63d10d521ebb263904b07b0e" }
@@ -74,6 +74,7 @@ test('GET | /dashboards', async (t) => {
 //   t.is(answer.status, 200);
 // });
 
+// Get dashboard and sources (not working)
 // test('GET | /dashboard', async (t) => {
 //     const token = jwtSign({ username: process.env.USERNAME, id: process.env.ID, email: process.env.EMAIL });
 //     const dash_id = { id: "63d10d521ebb263904b07b0e" };
@@ -86,6 +87,7 @@ test('GET | /dashboards', async (t) => {
 //   Tests for sources routes
 // */
 
+// get every available source
 test('GET | /sources', async (t) => {
   const token = jwtSign({ username: process.env.USERNAME, id: process.env.ID, email: process.env.EMAIL });
   const { statusCode, body } = await t.context.got(`sources/sources?token=${token}`);
