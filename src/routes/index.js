01dc7dfe61f16571cb242939740752d614427bca
Andreas Marios Kostopoulos <andreasmk00@gmail.com>
2023-02-21 20:54:32 +0200

    Added some comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,15 @@ const dashboards = require('./dashboards');
 const general = require('./general');
 const root = require('./root');
 
+// Setting up the router variable from express library
 const router = express.Router();
 
+// Rerouting accordingly
 router.use('/users', users);
 router.use('/sources', sources);
 router.use('/dashboards', dashboards);
 router.use('/general', general);
 router.use('/', root);
 
+// Export router
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -9,18 +9,28 @@ const router = express.Router();
 const User = require('../models/user');
 const Reset = require('../models/reset');
 
+/*
+POST /create
+Create user route
+*/
 router.post('/create',
   (req, res, next) => validation(req, res, next, 'register'),
   async (req, res, next) => {
+    
+    // Gathering information for the user from body
     const {username, password, email} = req.body;
     try {
       const user = await User.findOne({$or: [{username}, {email}]});
+      
+      // Check whether there is another user with the same email
       if (user) {
         return res.json({
           status: 409,
           message: 'Registration Error: A user with that e-mail or username already exists.'
         });
       }
+
+      // Create the new user
       const newUser = await new User({
         username,
         password,
@@ -32,24 +42,37 @@ router.post('/create',
     }
   });
 
+
+/*
+POST /authenticate
+Authenticate a user
+*/
 router.post('/authenticate',
   (req, res, next) => validation(req, res, next, 'authenticate'),
   async (req, res, next) => {
+    
+    // Gathering username and password from body
     const {username, password} = req.body;
     try {
       const user = await User.findOne({username}).select('+password');
+      
+      // Check whether the user exists
       if (!user) {
         return res.json({
           status: 401,
           message: 'Authentication Error: User not found.'
         });
       }
+      
+      // Check if the password is correct
       if (!user.comparePassword(password, user.password)) {
         return res.json({
           status: 401,
           message: 'Authentication Error: Password does not match!'
         });
       }
+      
+      // Authenticate and assign token
       return res.json({
         user: {
           username, 
@@ -63,18 +86,29 @@ router.post('/authenticate',
     }
   });
 
+
+/*
+POST /resetpassword
+Reset a password
+*/
 router.post('/resetpassword',
   (req, res, next) => validation(req, res, next, 'request'),
   async (req, res, next) => {
+    
+    // Gatherning information from body: username
     const {username} = req.body;
     try {
       const user = await User.findOne({username});
+      
+      // Check if the user exists
       if (!user) {
         return res.json({
           status: 404,
           message: 'Resource Error: User not found.'
         });
       }
+
+      // Resseting the token
       const token = jwtSign({username});
       await Reset.findOneAndRemove({username});
       await new Reset({
@@ -82,6 +116,7 @@ router.post('/resetpassword',
         token,
       }).save();
 
+      // Asking the user to enter their email in order to reset their password
       const email = mail(token);
       send(user.email, 'Forgot Password', email);
       return res.json({
@@ -93,14 +128,23 @@ router.post('/resetpassword',
     }
   });
 
+
+/*
+POST /changepassword
+Change password route
+*/
 router.post('/changepassword',
   (req, res, next) => validation(req, res, next, 'change'),
+  
+  // User authentication based on their username and password
   authorization,
   async (req, res, next) => {
     const {password} = req.body;
     const {username} = req.decoded;
     try {
       const user = await User.findOne({username});
+      
+      // Check if the user exists
       if (!user) {
         return res.json({
           status: 404,
@@ -108,12 +152,16 @@ router.post('/changepassword',
         });
       }
       const reset = await Reset.findOneAndRemove({username});
+      
+      // Check if the token has expired
       if (!reset) {
         return res.json({
           status: 410,
           message: ' Resource Error: Reset token has expired.'
         });
       }
+
+      // Setting up a new password
       user.password = password;
       await user.save();
       return res.json({
@@ -125,4 +173,5 @@ router.post('/changepassword',
     }
   });
 
+// Export router
 module.exports = router;
